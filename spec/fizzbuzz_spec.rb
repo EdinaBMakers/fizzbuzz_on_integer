b5adf17b8132641a02d3d12cb611e15a6f12d419
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-04-26 19:14:23 +0100

    Return fizz when called on a multiple of 3

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,6 +5,10 @@ describe 'fizzbuzz' do
     expect(3.fizzbuzz).to eq 'fizz'
   end
 
+  it 'returns "fizz" when called on a multiple of 3' do
+    expect(6.fizzbuzz).to eq 'fizz'
+  end
+
   it 'returns "buzz" when called on 5' do
     expect(5.fizzbuzz).to eq 'buzz'
   end
